b2037127f40e5c55983d393e70e9721ec79eac95
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2025-06-24 22:16:55 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        bw.write(fib()+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static long fib() {
+        long[] dp = new long[N + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[N];
+    }
+}
