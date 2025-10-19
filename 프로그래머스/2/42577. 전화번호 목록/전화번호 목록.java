426634e4b1563ce1bfb95e61a12ab82fbb4415e0
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2025-10-19 22:33:03 +0900

    [level 2] Title: 전화번호 목록, Time: 417.61 ms, Memory: 99.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public boolean solution(String[] pb) {
+        boolean answer = true;
+        
+        Arrays.sort(pb);
+        
+        for(int i = 0; i < pb.length - 1; i++){
+            if(pb[i+1].startsWith(pb[i])){
+                answer = false;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
