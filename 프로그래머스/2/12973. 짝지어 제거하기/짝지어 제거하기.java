c944222fd1c9189e96273e4bb3dbb92d06d93943
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-04-08 15:10:33 +0900

    [level 2] Title: 짝지어 제거하기, Time: 65.57 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if(stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        if(stack.isEmpty()) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
