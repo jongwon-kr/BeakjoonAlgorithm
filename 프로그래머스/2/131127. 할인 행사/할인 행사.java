efd7aa6c8460b932df9744cd66fd68355ce6311f
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-04-16 10:22:12 +0900

    [level 2] Title: 할인 행사, Time: 40.32 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,34 +3,35 @@ import java.io.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
 
+        int result = 0;
         Map<String, Integer> wMap = new HashMap<>();
-
-        for(int i = 0; i < want.length; i++) {
+        for (int i = 0; i < want.length; i++) {
             wMap.put(want[i], number[i]);
         }
 
-        for(int i = 0; i <= discount.length - 10; i++) {
-
-            Map<String, Integer> dMap = new HashMap<>();
-
-            for(int j = i; j < i + 10; j++) {
-                dMap.put(discount[j], dMap.getOrDefault(discount[j], 0) + 1);
-            }
-
-            if(isMatch(wMap, dMap)) {
-                answer++;
-            }
+        Map<String, Integer> window = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            window.put(discount[i], window.getOrDefault(discount[i], 0) + 1);
         }
-        return answer;
+        if (isMatch(wMap, window)) result++;
+
+        for (int i = 1; i <= discount.length - 10; i++) {
+            String prev = discount[i - 1];
+            String next = discount[i + 9];
+
+            window.put(prev, window.get(prev) - 1);
+            window.put(next, window.getOrDefault(next, 0) + 1);
+
+            if(isMatch(wMap, window)) result++;
+        }
+
+        return result;
     }
 
-    private boolean isMatch(Map<String, Integer> wMap, Map<String, Integer> dMap) {
-        for(String key : wMap.keySet()) {
-            if(dMap.getOrDefault(key, 0) < wMap.get(key)) {
-                return false;
-            }
+    private boolean isMatch(Map<String, Integer> wMap, Map<String, Integer> w) {
+        for (String key : wMap.keySet()) {
+            if(!w.getOrDefault(key, 0).equals(wMap.get(key))) return false;
         }
         return true;
     }
