60a1b42f85cb0bc894a3052d1e64ae4aa4ec9b6c
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-05-19 11:15:00 +0900

    [level 1] Title: 예산, Time: 0.44 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+
+        for (int i : d) {
+            budget -= i;
+            if (budget >= 0) {
+                answer++;
+            } else {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
