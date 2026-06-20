19dfb76cf187a9267e363e9a9d143e45ed71200f
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-06-20 12:40:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.76 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,24 +4,23 @@ import java.io.*;
 class Solution {
     public String solution(String[] p, String[] c) {
         String answer = "";
-        
+
         Map<String, Integer> map = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
         
-        for(String s : p){
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        for (int i = 0; i < p.length; i++) {
+            map.put(p[i], map.getOrDefault(p[i], 0) + 1);
         }
         
-        for(String s : c){
-            if(map.get(s) == 1){
-                map.remove(s);
-            }else{
-                map.put(s, map.get(s) - 1);
+        for (int i = 0; i < c.length; i++) {
+            if (map.containsKey(c[i])) {
+                map.put(c[i], map.get(c[i]) - 1);
             }
         }
         
-        for(String k : map.keySet()){
-            answer = k;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                answer = entry.getKey();
+            }
         }
         
         return answer;
