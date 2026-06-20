7ff8b8a418aa4e6f32891db77948d56a98b0ed7a
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-06-20 15:09:41 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,14 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
+        
         Map<String, Integer> map = new HashMap<>();
         
-        for(String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 1) + 1);
+        for (String[] cArr : clothes) {
+            map.put(cArr[1], map.getOrDefault(cArr[1], 1) + 1);
         }
-
-        for(int i : map.values()){
-            answer *= i;
+        
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            answer *= e.getValue();
         }
         
         return answer - 1;
