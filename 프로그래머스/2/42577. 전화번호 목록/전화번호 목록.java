42080e7bce97cbe3fd78cf546fc673698f345453
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-06-20 13:52:04 +0900

    [level 2] Title: 전화번호 목록, Time: 211.16 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,12 +4,15 @@ import java.io.*;
 class Solution {
     public boolean solution(String[] pb) {
         boolean result = true;
-        Arrays.sort(pb);
+        
+        // 배열의 최대 길이가 1000000이기 때문에 이중포문으로 두번 순회하면 시간 초과 발생
+        Arrays.sort(pb);    // 사전 순 정렬
         
         for (int i = 0; i < pb.length - 1; i++) {
-            if (pb[i + 1].startsWith(pb[i])) {
+            if(pb[i + 1].startsWith(pb[i])) {   // 현재 전화번호가 다음번에 올 전화번호의 접두사인지 확인
                 result = false;
-            }            
+                break;
+            }
         }
         
         return result;
