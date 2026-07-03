a166768437c9dbd01ff3d21d3a476bf614b16ee9
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-03 14:25:37 +0900

    [level 1] Title: 폰켓몬, Time: 2.41 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,16 +3,18 @@ import java.io.*;
 
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
         Set<Integer> set = new HashSet<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+
+        for (int i : nums) {
+            set.add(i);
         }
-        
-        if (set.size() > nums.length / 2) {
-            return nums.length / 2;
-        } else {
-            return set.size();
+
+        answer = set.size();
+
+        if (answer > nums.length / 2) {
+            answer = nums.length / 2;
         }
+        return answer;
     }
 }
