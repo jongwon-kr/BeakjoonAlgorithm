448e99369a35d634146ec355931a2af997814aff
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-21 23:07:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.97 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -4,23 +4,26 @@ import java.io.*;
 public class Solution {
     public int[] solution(int[] arr) {
         
-        Stack<Integer> s = new Stack<>();
+        // 스택, 큐 자료구조를 사용할때 공통적으로 ArrayDeque를 사용합니다.
+        ArrayDeque<Integer> arrDQ = new ArrayDeque<>();
         
         for (int i : arr) {
-            if (!s.isEmpty()) {
-                if (i != s.peek()) {
-                    s.push(i);
-                }
-            } else {
-                s.push(i);
+            
+            if (arrDQ.isEmpty()) {
+                arrDQ.offer(i);
+                continue;
+            }
+            
+            if (arrDQ.peekLast() != i) {
+                arrDQ.offer(i);
             }
         }
         
-        int[] result = new int[s.size()];
-        for (int i = s.size() - 1; i >= 0; i--) {
-            result[i] = s.pop();
+        int[] result = new int[arrDQ.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arrDQ.poll();
         }
-        
+ 
         return result;
     }
 }
