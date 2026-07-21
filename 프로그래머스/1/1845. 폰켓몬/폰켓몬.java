6f38c4c3828561dc2982300003c7cad7cb820e41
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-21 11:29:32 +0900

    [level 1] Title: 폰켓몬, Time: 2.59 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,17 +4,21 @@ import java.io.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
+        // 폰켓몬의 종류 구하기 Set 활용(중복 x)
         Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        int types = set.size();
+        int len = nums.length;
 
-        for (int i : nums) {
-            set.add(i);
+        // N/2보다 작은 경우 그 값이 최대값이고 그렇지 않으면 N/2를 반환
+        if (types <= len / 2) {
+            answer = types;
+        } else {
+            answer = len / 2;
         }
 
-        answer = set.size();
-
-        if (answer > nums.length / 2) {
-            answer = nums.length / 2;
-        }
         return answer;
     }
 }
