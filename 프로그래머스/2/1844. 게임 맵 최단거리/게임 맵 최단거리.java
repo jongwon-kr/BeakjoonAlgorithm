511c2ae48af46e1add4ad662f238acdd0089094e
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-07-24 17:35:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.82 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,47 @@
 import java.util.*;
 import java.io.*;
 
+
 class Solution {
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
-    int N, M;
+
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public int solution(int[][] maps) {
-        N = maps.length;
-        M = maps[0].length;
-        boolean[][] visited = new boolean[N][M];
 
-        return bfs(0, 0, maps, visited);
-    }
+        // 최단거리 BFS로 풀어야 함
+        // 벽이 존재하고 반환 값은 최단거리이거나 도달하지 못하면 -1 반환
+        // 시작지점 1,1 상대 진영 n,m
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int n = maps.length;
+        int m = maps[0].length;
+        int[][] dist = new int[n][m]; // 방문여부 or 거리 기록
 
-    public int bfs(int startX, int startY, int[][] maps, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startX, startY, 1});
-        visited[startX][startY] = true;
+        for (int[] arr : dist) {
+            Arrays.fill(arr, -1);
+        }
+
+        dist[0][0] = 0;
+        q.offer(new int[]{0, 0});
 
         while (!q.isEmpty()) {
+
             int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int dist = cur[2];
+            int x = cur[0], y = cur[1];
+            int distance = dist[x][y] + 1;
+
+            if (x == n - 1 && y == m - 1) {
+                return distance;
+            }
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = x + dx[i], ny = y + dy[i];
 
-                if (x == N - 1 && y == M - 1) {
-                   return dist;
-                }
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (maps[nx][ny] == 0 || dist[nx][ny] > -1) continue;
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-                    continue;
-                }
-
-                if(!visited[nx][ny] && maps[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    q.offer(new int[]{nx, ny, dist + 1});
-                }
+                dist[nx][ny] = distance;
+                q.offer(new int[]{nx, ny});
             }
         }
         return -1;
