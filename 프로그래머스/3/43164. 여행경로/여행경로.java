29c825036da4ced44148f7e23fc160ef4df63959
Bell_Hana <76871947+jongwon-kr@users.noreply.github.com>
2026-08-13 17:56:37 +0900

    [level 3] Title: 여행경로, Time: 0.39 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,35 +3,32 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[][] ts) {
-        String[] answer = {};
-        
-        List<String> result = new ArrayList<>(Arrays.asList(answer));
+
+        List<String> result = new ArrayList<>();
         Map<String, PriorityQueue<String>> map = new HashMap<>();
-        
-        for(int i = 0; i < ts.length; i++){
-            map.putIfAbsent(ts[i][0], new PriorityQueue<>());
-            map.get(ts[i][0]).add(ts[i][1]);
+
+        for (String[] t: ts) {
+            map.putIfAbsent(t[0], new PriorityQueue<>());
+            map.get(t[0]).add(t[1]);
         }
-        
-        
+
         dfs(result, map, "ICN");
-    
-        answer = new String[result.size()];
+
+        String[] answer = new String[result.size()];
         for(int i = 0; i < result.size(); i++){
             answer[i] = result.get(i);
         }
-        
+
         return answer;
     }
-    
+
     public void dfs(List<String> result, Map<String, PriorityQueue<String>> map, String from){
-        
+
         while(map.containsKey(from) && !map.get(from).isEmpty()){
             String str = map.get(from).poll();
-            System.out.println("str: " + str);
             dfs(result, map, str);
         }
-        
+
         result.add(0, from);
     }
 }
